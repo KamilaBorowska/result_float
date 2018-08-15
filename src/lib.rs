f8a8213ca174e2f87c241fc21ca4015ae7cda2d9
Konrad Borowski <konrad@borowski.pw>
2018-08-15 08:29:48 +0200

    Document ln

--- src/lib.rs
+++ src/lib.rs
@@ -562,6 +562,25 @@ where
         rfu(self.0.exp2())
     }
 
+    /// Returns the natural logarithm of the number.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn main() -> Result<(), result_float::NaN> {
+    /// use result_float::rf;
+    ///
+    /// let one = rf(1.0)?;
+    /// // e^1
+    /// let e = one.exp();
+    ///
+    /// // ln(e) - 1 == 0
+    /// let abs_difference = (e.ln()? - rf(1.0)?)?.abs();
+    ///
+    /// assert!(abs_difference < rf(1e-10)?);
+    /// # Ok(())
+    /// # }
+    /// ```
     #[inline]
     pub fn ln(self) -> Result<F> {
         rf(self.0.ln())
